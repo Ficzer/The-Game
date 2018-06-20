b8aa0f687795e831483e8204c15b86b247ac08a6
DESKTOP-REAPV8S\Filip <filipflorczyk@gmail.com>
2018-06-20 14:01:41 +0200

    Test of SFML
    Test of SFML.NET 2.2 64bit

---
+++ RPG-Game/RPG-Game/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace RPG_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(1000, 800), "Test", Styles.Default);
+
+            window.Closed += (sender, arg) => window.Close();
+
+            while(window.IsOpen)
+            {
+                window.DispatchEvents();
+
+
+                window.Clear();
+                window.Display();
+            }
+            
+        }
+    }
+}
